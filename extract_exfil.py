740de134f1a7b607496006da39a830db06974153
Anisha Pai <pai22m@mtholyoke.edu>
2020-04-29 18:21:57 -0400

    extraction file updated

--- extract_exfil.py
+++ extract_exfil.py
@@ -1,11 +1,26 @@
 from scapy import *
-import base64
+import argparse
 
-capture = rdpcap('data.pcap')
-ping_data = ""
+def main():
+    parser.add_argument('-f',help='pcap file',required=True)  ## archer data file path
+    argz = parser.parse_args()
+    file = argz.f.strip()
+    key = "BeyWfkPjXCERVUCXUIhEypFFYesgqPfKjLfEhZEqKTnolrrcekCJdGQtdVgnbXcBqkPcGyOEIrmpgbhqJjfMIClzdwNmmhhrHPJXIqCtdyXlmrlEyWmsIPvGHrlGmBkkdLKjRhNxueKCzRWaMiTUYTHHwsfvaNBShLUwmgXLQulWNBjiVOAwsTWjDCCBhrGQHJNCgUKBIneOjZsKZMHRPQecKRFLVksEvRDFnOmJihvTwIRuZgiZuLUiBwCxVjrbvbaNnKqdWuUPJFtfAlADmelZQYQoRaNaKZCeYWtmnzdgJdRlvS"
 
-for packet in capture:
-   if packet[ICMP].type == 0: # Echo request
-       ping_data += packet.load
+    data = get_data(file)
+    data = key ^ data
+    print(data)
 
-print(ping_data)
+def getdata(file):
+    capture = rdpcap(file)
+    ping_data = ""
+
+    for packet in capture:
+       if packet[ICMP].type == 0: # Echo request
+           ping_data += packet.load
+
+    return ping_data
+
+
+if __name__ == "__main__":
+    main()
