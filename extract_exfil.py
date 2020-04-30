63a537b41dfbfd74c8c0cce4c655f53c8ea9f942
Anisha Pai <pai22m@mtholyoke.edu>
2020-04-30 11:21:49 -0400

    updated extract_exfil

--- extract_exfil.py
+++ extract_exfil.py
@@ -1,24 +1,25 @@
-from scapy import *
+from scapy.all import *
 import argparse
+from itertools import cycle
 
 def main():
+    parser = argparse.ArgumentParser()
     parser.add_argument('-f',help='pcap file',required=True)  ## archer data file path
     argz = parser.parse_args()
     file = argz.f.strip()
-    key = "BeyWfkPjXCERVUCXUIhEypFFYesgqPfKjLfEhZEqKTnolrrcekCJdGQtdVgnbXcBqkPcGyOEIrmpgbhqJjfMIClzdwNmmhhrHPJXIqCtdyXlmrlEyWmsIPvGHrlGmBkkdLKjRhNxueKCzRWaMiTUYTHHwsfvaNBShLUwmgXLQulWNBjiVOAwsTWjDCCBhrGQHJNCgUKBIneOjZsKZMHRPQecKRFLVksEvRDFnOmJihvTwIRuZgiZuLUiBwCxVjrbvbaNnKqdWuUPJFtfAlADmelZQYQoRaNaKZCeYWtmnzdgJdRlvS"
-
-    data = get_data(file)
-    data = key ^ data
+    key = b"BeyWfkPjXCERVUCXUIhEypFFYesgqPfKjLfEhZEqKTnolrrcekCJdGQtdVgnbXcBqkPcGyOEIrmpgbhqJjfMIClzdwNmmhhrHPJXIqCtdyXlmrlEyWmsIPvGHrlGmBkkdLKjRhNxueKCzRWaMiTUYTHHwsfvaNBShLUwmgXLQulWNBjiVOAwsTWjDCCBhrGQHJNCgUKBIneOjZsKZMHRPQecKRFLVksEvRDFnOmJihvTwIRuZgiZuLUiBwCxVjrbvbaNnKqdWuUPJFtfAlADmelZQYQoRaNaKZCeYWtmnzdgJdRlvS"
+    data = getdata(file)
+    data = ''.join(chr(x ^ y) for (x,y) in zip(data, cycle(key)))
     print(data)
 
 def getdata(file):
     capture = rdpcap(file)
-    ping_data = ""
+    ping_data = b""
 
     for packet in capture:
-       if packet[ICMP].type == 0: # Echo request
+       if ICMP in packet: # Echo request
            ping_data += packet.load
-
+    ping_data = ping_data[8:]
     return ping_data
 
 
